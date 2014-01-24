e6d000ed0fca1879f96a7d98c7cb09085b5d5f6e
FABIO ARIAS <fharias@gmail.com>
2014-01-24 11:36:26 -0500

    REST Ful APi

---
+++ app/Controller/ServersController.php
@@ -0,0 +1,41 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+class ServerController extends AppController {
+
+    public $components = array(
+        'RequestHandler',
+        'Rest.Rest' => array(
+            'catchredir' => true, // Recommended unless you implement something yourself
+            'debug' => 0,
+            'actions' => array(
+                'view' => array(
+                    'extract' => array('server.Server' => 'servers.0'),
+                ),
+                'index' => array(
+                    'extract' => array('rows.{n}.Server' => 'servers'),
+                ),
+            ),
+        ),
+    );
+
+    /**
+     * Shortcut so you can check in your Controllers wether
+     * REST Component is currently active.
+     *
+     * Use it in your ->flash() methods
+     * to forward errors to REST with e.g. $this->Rest->error()
+     *
+     * @return boolean
+     */
+    protected function _isRest() {
+        return !empty($this->Rest) && is_object($this->Rest) && $this->Rest->isActive();
+    }
+
+}
+
+?>
