0a400a40a6f5cedb8c4b0c6d99d2d2986457a93b
FABIO ARIAS <fharias@gmail.com>
2014-01-24 11:58:21 -0500

    Primeros métodos REST

--- app/Controller/ServersController.php
+++ app/Controller/ServersController.php
@@ -6,42 +6,14 @@
  */
 
 class ServersController extends AppController {
-
-    public $components = array(
-        'RequestHandler',
-        'Rest.Rest' => array(
-            'catchredir' => true, // Recommended unless you implement something yourself
-            'debug' => 0,
-            'actions' => array(
-                'view' => array(
-                    'extract' => array('server.Server' => 'servers.0'),
-                ),
-                'index' => array(
-                    'extract' => array('rows.{n}.Server' => 'servers'),
-                ),
-            ),
-        ),
-    );
-
-    /**
-     * Shortcut so you can check in your Controllers wether
-     * REST Component is currently active.
-     *
-     * Use it in your ->flash() methods
-     * to forward errors to REST with e.g. $this->Rest->error()
-     *
-     * @return boolean
-     */
-    protected function _isRest() {
-        return !empty($this->Rest) && is_object($this->Rest) && $this->Rest->isActive();
-    }
-    
-    public function index(){
-        
-    }
-    
-    public function view(){
-        
+    public $components = array('RequestHandler');
+    function index(){
+        $this->loadModel('Bin');
+        $data = $this->Bin->find('all');
+        $this->set(array(
+            'Bin' => $data,
+            '_serialize' => array('Bin')
+        ));
     }
     
 }
