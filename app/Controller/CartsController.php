253bec97b4e3399f39ceeaa9e36d27899899b30f
FABIO ARIAS <fharias@gmail.com>
2014-02-12 15:12:02 -0500

    ….

--- app/Controller/CartsController.php
+++ app/Controller/CartsController.php
@@ -39,12 +39,9 @@ class CartsController extends AppController{
     
     public function show($uuid){
         $this->loadModel('Cart');
-        $cart = $this->Cart->query("select a.*, b.*, c.Description1, c.Description2, c.Cost from Cart a, CartItem b, Item c where a.id = b.cart_id and c.Code = b.ItemCode and a.uuid = '".$uuid."' and a.state=1");
+        $cart = $this->Cart->query("select a.*, b.*, c.Description1, c.Description2, c.Cost from Cart a, CartItem b, Item c where a.id = b.cart_id and c.Code = b.ItemCod and a.uuid = '".$uuid."' and a.state=1");
         
-        $this->set(array(
-            'response' => $cart,
-            '_serialize' => array('response')
-        ));
+        $this->set('response',$cart);
     }
     
     public function checkout($uuid, $vendorcode, $factura){
